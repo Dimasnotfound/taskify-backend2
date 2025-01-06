fa33a88be593a9f030ce305f87e24f9faf6cd5a3
Dimasnotfound <dp4369344@gmail.com>
2025-01-06 13:02:11 +0700

    add:menambahkan handle 500

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,29 +15,24 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
-        // Validasi data input
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8',
         ]);
 
-        // Return error jika validasi gagal
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
-        // Buat pengguna baru
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        // Buat token untuk pengguna
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        // Return data pengguna dan token
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
@@ -45,37 +40,27 @@ class AuthController extends Controller
         ], 201);
     }
 
-    /**
-     * Handle user login.
-     */
     public function login(Request $request)
     {
-        // Validasi data input
         $validator = Validator::make($request->all(), [
             'email' => 'required|string|email',
             'password' => 'required|string',
         ]);
 
-        // Return error jika validasi gagal
         if ($validator->fails()) {
             return response()->json(['errors' => $validator->errors()], 422);
         }
 
-        // Ambil kredensial dari request
         $credentials = $request->only('email', 'password');
 
-        // Periksa kredensial
         if (!Auth::attempt($credentials)) {
             return response()->json(['message' => 'Invalid credentials'], 401);
         }
 
-        // Ambil pengguna yang sedang login
         $user = Auth::user();
 
-        // Buat token untuk pengguna
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        // Return data pengguna dan token
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
