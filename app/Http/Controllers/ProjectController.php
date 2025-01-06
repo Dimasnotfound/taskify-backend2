22b3bd16e324314912438e7cda34f17f3fb7baaf
Dimasnotfound <dp4369344@gmail.com>
2025-01-06 12:19:53 +0700

    add: menambahkan route untuk 404 dan menambahkan route api untuk menadaptkan project summary

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Task;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
@@ -29,5 +30,24 @@ class ProjectController extends Controller
         ]);
 
         return response()->json($project, 201);
+    }
+
+    public function summary()
+    {
+        $userId = Auth::id();
+        $summary = Task::selectRaw("
+                SUM(CASE WHEN status = 'ready' THEN 1 ELSE 0 END) as ready,
+                SUM(CASE WHEN status = 'in_progress' THEN 1 ELSE 0 END) as in_progress,
+                SUM(CASE WHEN status = 'on_hold' THEN 1 ELSE 0 END) as on_hold,
+                SUM(CASE WHEN status = 'in_review' THEN 1 ELSE 0 END) as in_review,
+                SUM(CASE WHEN status = 'done' THEN 1 ELSE 0 END) as done
+            ")
+            ->where('assigned_to', $userId)
+            ->first();
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $summary,
+        ]);
     }
 }
